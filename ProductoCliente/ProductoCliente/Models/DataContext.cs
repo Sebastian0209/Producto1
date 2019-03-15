f4e995e275ab37bdf973a4aef94316d7c6b03fd9
Sebastian <sebastianriosanchez68@gmail.com>
2019-03-15 13:00:06 -0400

    Entity framework instaladp

---
+++ ProductoCliente/ProductoCliente/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductoCliente.Models
+{
+    public class DataContext: DbContext
+    {
+
+    }
+}
